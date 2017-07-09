e81a64f860c537a3f520f7033f35a41d82960d9a
Ekilei <ekilei@gmail.com>
2017-07-09 11:45:07 +0300

    первое описание

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
 # yii2-numstepper-widget
-Numstepper for adaptive site
+
+Numstepper является надстройкой над InputWidget, предназначен для адаптивных сайтов, удобен в применении на декстопе и на мобильных устройствах.
+
+Numstepper is an add-on above the InputWidget, designed for adaptive sites, convenient for use on the desktop and on mobile devices.
+
+
+```angular2html
+<?= $form->field($model,'day')->widget(\common\widgets\Numstepper::className()) ?>
+?>
+```
+
+### C параметрами
+With parameters
+
+![ScreenShot](https://raw.github.com/ekilei/yii2-numstepper-widget/master/screen/1.png)
+
+```angular2html
+<?= $form->field($model,'day')->widget(\common\widgets\Numstepper::className(),
+    [
+        'min' => -28,
+        'max' => 28,
+    ])
+?>
+```
+
+### C исключением и значением при инициализации
+With exception and initialization value
+```angular2html
+<?= $form->field($model,'day')->widget(\common\widgets\Numstepper::className(),
+    [
+        'min' => -28,
+        'max' => 28,
+        'exclude' => [0],
+        'default' => (int)$model->day ? (int)$model->day : 1,
+    ])
+?>
+```
+
+### C подсказкой
+With a hint
+
+![ScreenShot](https://raw.github.com/ekilei/yii2-numstepper-widget/master/screen/2.png)
+
+```angular2html
+<?= $form->field($model,'day')->widget(\common\widgets\Numstepper::className(),
+[
+    'min' => -28,
+    'max' => 28,
+    'exclude' => [0],
+    'default' => (int)$model->day ? (int)$model->day : 1,
+    ])
+     ->hint(Yii::t('app','Negative numbers make it possible to choose a day from the end of the month.').'
+            <br>'.Yii::t('app','Example').': "-1" = [31,30,28(29)], а "-3" = [29,28,26(27)] ') 
+?>
+```
+
+### Cо своими иконками кнопок
+With own icon buttons
+
+![ScreenShot](https://raw.github.com/ekilei/yii2-numstepper-widget/master/screen/3.png)
+
+```angular2html
+<?= $form->field($model,'day')->widget(\common\widgets\Numstepper::className(),
+    [
+        'default' => (int)$model->day ? (int)$model->day : 1,
+        'minusButton' => 'menu-down',
+        'plusButton' => 'menu-up',
+    ])
+?>
+```
+
+### С изменением расположением кнопок      
+With the change in the arrangement of the buttons
+     
+![ScreenShot](https://raw.github.com/ekilei/yii2-numstepper-widget/master/screen/4.png)
+              
+```angular2html
+<?= $form->field($model,'day')->widget(\common\widgets\Numstepper::className(),
+    [
+        'default' => (int)$model->day ? (int)$model->day : 1,
+        'minusButton' => 'menu-down',
+        'plusButton' => 'menu-up',
+        'prepend' => [],
+        'append' => ['{minus}','{plus}'],
+    ])
+?>
+```
---
+++ common/widgets/Numstepper.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * created by Ekilei <ekilei@rusoft.ru>
+ */
+namespace common\widgets;
+
+use Yii;
+use yii\helpers\Html;
+
+class Numstepper extends \yii\widgets\InputWidget
+{
+    public $minusButton = 'minus';
+    public $plusButton = 'plus';
+
+    public $min = null;
+    public $max = null;
+    public $step = 1;
+    public $default = 0;
+    public $exclude = [];
+
+    public $append = '{plus}';
+    public $prepend = '{minus}';
+
+    public function init()
+    {
+        parent::init();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        Html::addCssClass($this->options, 'form-control');
+        $addon = false;
+        $html = '';
+
+        {
+            $input = $this->hasModel()
+                ? Html::activeTextInput($this->model, $this->attribute, $this->options)
+                : Html::textInput($this->name, $this->value, $this->options);
+            $prepend = $this->renderAddon($this->prepend);
+            $append = $this->renderAddon($this->append);
+            if ($prepend !== '' || $append !== '') {
+                $addon = true;
+            }
+        }
+
+        if ($addon) {
+            $html .= Html::beginTag('div', ['class' => 'input-group numstepper']);
+            $html .= $prepend;
+        }
+        $html .= $input;
+        if ($addon) {
+            $html .= $append;
+            $html .= Html::endTag('div');
+        }
+        $this->registerPlugin();
+        return $html;
+    }
+
+    protected function renderAddon($addons)
+    {
+        if ($addons === false) {
+            return '';
+        }
+        if (!is_array($addons)) {
+            $addons = [$addons];
+        }
+        if (!count($addons)) {
+            return '';
+        }
+        $result= '';
+        foreach ($addons as $addon) {
+            if ($addon === '{plus}') {
+                $result .= Html::tag('span', "<span class=\"glyphicon glyphicon-".$this->plusButton."\"></span>", ['class' => 'btn input-group-addon plusbutton']);
+            } elseif ($addon === '{minus}') {
+                $result .= Html::tag('span', "<span class=\"glyphicon glyphicon-".$this->minusButton."\"></span>", ['class' => 'btn input-group-addon minusbutton']);
+            }
+        }
+
+        return $result;
+    }
+
+    protected function registerPlugin()
+    {
+        $view = $this->getView();
+
+        $id = $this->options['id'];
+
+        $selector = "jQuery('#$id')";
+        $selector .= '.closest(".input-group.numstepper")';
+
+        $js = "$selector.find('.minusbutton').on('click', function() {
+            var num = parseFloat($('#$id').val());
+            num = num-$this->step;
+            ".($this->exclude ? "while(-1 != $.inArray(num,[".implode(',',$this->exclude)."])){
+            num = num-$this->step;
+            }" : "")."
+            if(!num) num = $this->default;
+            if($this->min != null && $this->min > num) num = $this->min; 
+            if($this->min != null && $this->max < num) num = $this->max; 
+            $('#$id').val(num);
+        });
+        $selector.find('.plusbutton').on('click', function() {
+            var num = parseFloat($('#$id').val());
+            num = num+$this->step;
+            ".($this->exclude ? "while(-1 != $.inArray(num,[".implode(',',$this->exclude)."])){
+            num = num+$this->step;
+            }" : "")."
+            if(!num) num = $this->default;
+            if($this->min != null && $this->min > num) num = $this->min; 
+            if($this->min != null && $this->max < num) num = $this->max; 
+            $('#$id').val(num);
+        });
+        $('#$id').on('change',function(){
+             ".($this->exclude ? "if(0 <= $.inArray(parseFloat($('#$id').val()),[".implode(',',$this->exclude)."])) {
+            $('#$id').val('$this->default');
+            }" : '')."
+        });
+        ";
+
+        $js .=  "jQuery('#$id').val(".$this->default.")";
+        $view->registerJs($js);
+
+    }
+
+
+
+}
